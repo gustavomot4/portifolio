bdc149f62175662f2fdf65fbefce793ace3d8e3e
gustavomot4 <gustavo.mota@sptech.school>
2026-03-09 09:24:32 -0300

    Construção do hero do portifólio

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function ChamarLinks(index){
+
+    if(index.id == "linkedin"){
+        window.open("https://www.linkedin.com/in/gustavoalvesmota/", "_blank");
+    }
+    
+    if(index.id == "github"){
+         window.open("https://github.com/gustavomot4", "_blank");
+    }
+
+}
